adf59fcd6b91cc90f04c0088d68a8a136dac21c8
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-09-29 21:33:59 -0400

    Add commented-out lines for 'AUTH_USER_MODEL' foreign key.

--- prints/models.py
+++ prints/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+# from print_tracker.settings.common import AUTH_USER_MODEL
+
 
 class ModelPrint(models.Model):
     name = models.CharField(max_length=255)
+    # creator = models.ForeignKey(AUTH_USER_MODEL, related_name='prints', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.id} : {self.name}'
