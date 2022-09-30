0b0dc035408a255b2fcfa72ef9a72cc5f746cdf1
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-09-30 04:19:23 -0400

    Change 'filament' to 'null=True' to troubleshoot 'ModelPrint' view issue..

--- prints/models.py
+++ prints/models.py
@@ -13,7 +13,7 @@ class Filament(models.Model):
 class ModelPrint(models.Model):
     name = models.CharField(max_length=255)
     creator = models.ForeignKey(AUTH_USER_MODEL, related_name='prints', on_delete=models.CASCADE)
-    filament = models.ForeignKey(Filament, related_name='prints', on_delete=models.PROTECT)
+    filament = models.ForeignKey(Filament, related_name='prints', null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'{self.id} : {self.name} : {self.creator.username} : {self.filament.material}'
