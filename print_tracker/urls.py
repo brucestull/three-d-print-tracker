571ad94409ae9ac42057981f823044fb83d99bd3
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-09-29 21:32:08 -0400

    Add prints URL config to root URL config.

--- print_tracker/urls.py
+++ print_tracker/urls.py
@@ -20,8 +20,12 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="home.html"), name='home'),
+
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
+
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
+
+    path('prints/', include('prints.urls'), name='prints'),
 ]
