6996fa120ff05bd604e09c6f3c6089fefe42325e
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-09-30 15:20:19 -0400

    Add URL for 'UpdateView'.

--- prints/urls.py
+++ prints/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.ModelPrintListView.as_view(), name='home'),
     path('print/create/', views.ModelPrintCreateView.as_view(), name='model_create'),
     path('print/<int:pk>/', views.ModelPrintDetailView.as_view(), name='model_detail'),
+    path('print/<int:pk>/edit/', views.ModelPrintUpdateView.as_view(), name='model_update'),
 ]
