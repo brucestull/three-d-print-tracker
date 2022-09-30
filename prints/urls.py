585f2f47d6acba7073b5e33377f481ed9bd35468
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-09-30 14:38:34 -0400

    Rename 'model_prints' view to 'home'

--- prints/urls.py
+++ prints/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'prints'
 urlpatterns = [
-    path('', views.ModelPrintListView.as_view(), name='model_prints'),
+    path('', views.ModelPrintListView.as_view(), name='home'),
+    path('print/create/', views.ModelPrintCreateView.as_view(), name='model_create'),
     path('print/<int:pk>/', views.ModelPrintDetailView.as_view(), name='model_detail'),
 ]
