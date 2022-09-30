59ce727e3b0b697b5e2d4a1fb1c3e405a3e907b3
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-09-29 21:34:46 -0400

    Add 'ListView' 'ModelPrintListView'.

--- prints/views.py
+++ prints/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import ModelPrint
+
+
+class ModelPrintListView(ListView):
+    """
+    Class-based view, which inherits from `django.views.generic.ListView`, to provide list view of model `ModelPrint`.
+    """
+    model = ModelPrint
+    template_name = 'model_print_list.html'
