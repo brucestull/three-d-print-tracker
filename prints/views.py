e0a62eab99ef2813c15e9ab13dbc77879921326e
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-10-01 05:36:08 -0400

    Commit to preserve comments regarding 'self.request' and 'form'.

--- prints/views.py
+++ prints/views.py
@@ -1,9 +1,13 @@
 from django.urls import reverse_lazy
+
 from django.views.generic import ListView
 from django.views.generic import DetailView
+
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from prints.models import ModelPrint
 
@@ -21,10 +25,41 @@ class ModelPrintDetailView(DetailView):
     template_name ='model_print_detail.html'
 
 
-class ModelPrintCreateView(CreateView):
+class ModelPrintCreateView(LoginRequiredMixin, CreateView):
     model = ModelPrint
     template_name ='model_print_create.html'
-    fields = ['name', 'creator', 'filament']
+    fields = ['name', 'filament']
+
+    def form_valid(self, form):
+
+        # print('self: ', self)
+        # <prints.views.ModelPrintCreateView object at 0x00000242A0E4CDC0>
+        # print('form: ', form)
+        # Some HTML form-looking thing.
+
+        # print('self.request: ', self.request)
+        # self.request:  <WSGIRequest: POST '/prints/print/create/'>
+
+        # print('self.request.keys(): ', self.request.keys())
+        # Breaks, no such 'keys()' thing.
+
+        # print('self.request.POST: ', self.request.POST)
+        """
+        self.request.POST:  <QueryDict: {
+            'csrfmiddlewaretoken': ['IkmVDHQMZhPYF4sH69lqTsca58DZqMBvFX9L856UOfCAnkWoi1uDt9QJPD3C2Zu0'],
+            'name': ['A Model Print Name'],
+            'creator': ['1'],
+            'filament': ['10']
+        }>
+        """
+
+        # print('self.request.user: ', self.request.user)
+        # self.request.user:  admin
+        # print('type(self.request.user): ', type(self.request.user))
+        # type(self.request.user):  <class 'django.utils.functional.SimpleLazyObject'>
+
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
 
 
 class ModelPrintUpdateView(UpdateView):
