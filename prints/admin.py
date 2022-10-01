86764e3e0e98e29236db28101c597f53556c979a
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-10-01 18:59:39 -0400

    Commit to save experiment with multiple database dependencies.

--- prints/admin.py
+++ prints/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 
 from .models import ModelPrint
-from .models import Filament
+from .models import FilamentInstance
+from .models import FilamentRoll
 from .models import Manufacturer
 
 
 admin.site.register(ModelPrint)
-admin.site.register(Filament)
+admin.site.register(FilamentInstance)
+admin.site.register(FilamentRoll)
 admin.site.register(Manufacturer)
 
 
--- prints/models.py
+++ prints/models.py
@@ -14,21 +14,55 @@ class Manufacturer(models.Model):
         return f'{self.id} : {self.name}'
 
 
-class Filament(models.Model):
+def get_or_create_a_deleted_manufacturer():
+    """
+    Gets an existing `Manufacturer` object or creates a new `Manufacturer` object which has `name` attribute of `deleted`. The [0] gets the first element of the QuerySet.
+    """
+    return Manufacturer.objects.get_or_create(name='deleted')[0]
+
+
+class FilamentRoll(models.Model):
     """
     Model for the `Filament` used in `ModelPrint`.
     """
     material = models.CharField(max_length=255)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        related_name='manufacturers',
+        on_delete=models.SET(
+            get_or_create_a_deleted_manufacturer
+        )
+    )
 
     def __str__(self):
-        return f'{self.id} : {self.material}'
+        return f'{self.id} : {self.material} : {self.manufacturer.name}'
 
 
-def get_or_create_a_deleted_filament():
+def get_or_create_a_deleted_filament_roll():
     """
-    Gets an existing `Filament` object or creates a new `Filament` object which has `material` attribute of `deleted`. The [0] gets the first element of the QuerySet.
+    Gets an existing `FilamentRoll` object or creates a new `FilamentRoll` object which has `manufacturer` attribute of `deleted`. The [0] gets the first element of the QuerySet.
     """
-    return Filament.objects.get_or_create(material='deleted')[0]
+    return FilamentRoll.objects.get_or_create(manufacturer='deleted')[0]
+
+class FilamentInstance(models.Model):
+    """
+    Instances of `Filament`. The actual filament consumed by a model print.
+    """
+    filament_used = models.FloatField(default=0)
+    filament_roll = models.ForeignKey(
+        FilamentRoll,
+        related_name='rolls',
+        on_delete=models.SET(
+            get_or_create_a_deleted_filament_roll
+        )
+    )
+
+
+def get_or_create_a_deleted_filament_instance():
+    """
+    Gets an existing `FilamentInstance` object or creates a new `FilamentInstance` object which has `filament_roll` attribute of `deleted`. The [0] gets the first element of the QuerySet.
+    """
+    return FilamentInstance.objects.get_or_create(filament_roll='deleted')[0]
 
 
 class ModelPrint(models.Model):
@@ -42,20 +76,20 @@ class ModelPrint(models.Model):
         related_name='prints',
         on_delete=models.CASCADE
     )
-    filament = models.ForeignKey(
-        Filament,
+    filament_instance = models.ForeignKey(
+        FilamentInstance,
         related_name='prints',
         on_delete=models.SET(
-            get_or_create_a_deleted_filament
+            get_or_create_a_deleted_filament_instance
         )
     )
 
     def __str__(self):
         return (
-            f'{self.id} : '
             f'{self.name} : '
             f'{self.creator.username} : '
-            f'{self.filament.material if self.filament else "No filament provided"}'
+            f'{self.filament_instance.filament_roll.material if self.filament_instance.filament_roll else "No filament provided"} : '
+            f'{self.id}'
         )
     
     def get_absolute_url(self):
