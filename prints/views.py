fb5c1826d57aa8cb7b260e35ff6557b7169c12f1
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-10-01 05:45:02 -0400

    Remove note comments

--- prints/views.py
+++ prints/views.py
@@ -31,33 +31,9 @@ class ModelPrintCreateView(LoginRequiredMixin, CreateView):
     fields = ['name', 'filament']
 
     def form_valid(self, form):
-
-        # print('self: ', self)
-        # <prints.views.ModelPrintCreateView object at 0x00000242A0E4CDC0>
-        # print('form: ', form)
-        # Some HTML form-looking thing.
-
-        # print('self.request: ', self.request)
-        # self.request:  <WSGIRequest: POST '/prints/print/create/'>
-
-        # print('self.request.keys(): ', self.request.keys())
-        # Breaks, no such 'keys()' thing.
-
-        # print('self.request.POST: ', self.request.POST)
         """
-        self.request.POST:  <QueryDict: {
-            'csrfmiddlewaretoken': ['IkmVDHQMZhPYF4sH69lqTsca58DZqMBvFX9L856UOfCAnkWoi1uDt9QJPD3C2Zu0'],
-            'name': ['A Model Print Name'],
-            'creator': ['1'],
-            'filament': ['10']
-        }>
+        Set `form.instance.creator` as `self.request.user` when valid form data is provided.
         """
-
-        # print('self.request.user: ', self.request.user)
-        # self.request.user:  admin
-        # print('type(self.request.user): ', type(self.request.user))
-        # type(self.request.user):  <class 'django.utils.functional.SimpleLazyObject'>
-
         form.instance.creator = self.request.user
         return super().form_valid(form)
 
