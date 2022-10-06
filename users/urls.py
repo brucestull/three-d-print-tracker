46657ea25fe66a36430bb5bb19a2aacea2ca9b52
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-10-06 12:52:56 -0400

    Add user update view.

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from users.views import SignUpView
+from users.views import UserUpdateView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("<int:pk>/edit/", UserUpdateView.as_view(), name="edit_profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
 
 from users.forms import CustomUserCreationForm
+from users.forms import CustomUserChangeForm
+
+from users.models import CustomUser
 
 class SignUpView(CreateView):
     """
@@ -10,3 +14,13 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+class UserUpdateView(UpdateView):
+    """
+    View for user to update an existing account.
+    """
+    model = CustomUser
+    form_class = CustomUserChangeForm
+    success_url = reverse_lazy('login')
+    template_name ='registration/update.html'
