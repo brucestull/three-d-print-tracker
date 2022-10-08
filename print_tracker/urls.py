ad54666c1c91691774b51a15ae60d8f056871be6
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-10-08 06:19:45 -0400

    Remove 'home.html', add 'RedirectView'.

--- print_tracker/urls.py
+++ print_tracker/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="home.html"), name='home'),
+    path('', RedirectView.as_view(pattern_name='prints:models'), name='home'),
 
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
