9f11d6ecc8cd914bc451e5472b6d5e14fd60f5d8
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-10-10 05:29:17 -0400

    Add basic endpoint for 'ModelPrint'.

--- api/views.py
+++ api/views.py
@@ -1,14 +1,19 @@
 from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
+
 from rest_framework import viewsets
 from rest_framework import permissions
 
-from .serializers import UserSerializer, GroupSerializer
-from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+from .serializers import GroupSerializer
+from .serializers import BasicModelPrintSerializer
+
+from prints import models
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows `users` to be viewed or edited.
     """
     queryset = get_user_model().objects.all().order_by('-date_joined')
     # queryset = get_user_model().objects.all().order_by('email')
@@ -18,8 +23,17 @@ class UserViewSet(viewsets.ModelViewSet):
 
 class GroupViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows groups to be viewed or edited.
+    API endpoint that allows `groups` to be viewed or edited.
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class BasicModelPrintViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows `ModelPrints` to be viewed or edited.
+    """
+    queryset = models.ModelPrint.objects.all()
+    serializer_class = BasicModelPrintSerializer
     permission_classes = [permissions.IsAuthenticated]
