0d55909565d0f3aaf9f34e225be45b4856045e7c
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2022-10-20 10:44:18 -0400

    Revert "Add 'UserPassesTestMixin' and 'test_func()' to 'FilamentInstanceUpdateView'."
    This reverts commit 2245b96e1cb691b402dc85d1448e1dff4e2ead13.

--- prints/views.py
+++ prints/views.py
@@ -147,19 +147,13 @@ class FilamentInstanceCreateView(LoginRequiredMixin, CreateView):
     ]
 
 
-class FilamentInstanceUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class FilamentInstanceUpdateView(LoginRequiredMixin, UpdateView):
     model = FilamentInstance
     template_name = 'filament_instances/filament_instance_edit.html'
     fields = [
         'grams_filament_consumed',
         'filament_roll',
     ]
-
-    def test_func(self):
-        filament_instance = self.get_object()
-        print('filament_instance: ', filament_instance)
-        print('Can we update filament_instance?: ', self.request.user == filament_instance.print.creator)
-        return self.request.user == filament_instance.print.creator
 
 
 class FilamentInstanceDeleteView(LoginRequiredMixin, DeleteView):
